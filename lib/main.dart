7f95d141786ab10026145e8c32b8bb6b6a6afd66
gabrielgasparotto <gabriel.gasparotto01@gmail.com>
2019-08-13 11:17:59 -0300

    Metodos de adiçao e remoçao de tarefa na lista

--- lib/main.dart
+++ lib/main.dart
@@ -35,27 +35,63 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var novaTarefa = TextEditingController();
+
+  void adicionaTarefa(){
+    setState((){
+      if(novaTarefa.text.isNotEmpty){
+        widget.items.add(Item(title: novaTarefa.text, done: false));
+        novaTarefa.clear();
+      }
+    });
+  }
+
+  void removeTarefa(int index){
+    setState(() {
+      widget.items.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Todo List"),
+        title: TextFormField(
+          controller: novaTarefa,
+          keyboardType: TextInputType.text,
+          style: TextStyle(color: Colors.white, fontSize: 18),
+          decoration: InputDecoration(
+              labelText: "Nova Tarefa",
+              labelStyle: TextStyle(color: Colors.white)
+          ),
+        ),
       ),
       body: ListView.builder(
         itemCount: widget.items.length,
         itemBuilder: (BuildContext context, int index) {
           final item = widget.items[index];
-          return CheckboxListTile(
-            title: Text(item.title),
+          return Dismissible(
             key: Key(item.title),
-            value: item.done,
-            onChanged: (value) {
-              setState(() {
-                item.done = value;
-              });
+            background: Container(color: Colors.red.withOpacity(0.2)),
+            onDismissed: (direction) {
+              removeTarefa(index);
             },
+            child: CheckboxListTile(
+              title: Text(item.title),
+              value: item.done,
+              onChanged: (value) {
+                setState(() {
+                  item.done = value;
+                });
+              },
+            ),
           );
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: adicionaTarefa,
+        child: Icon(Icons.add),
+        backgroundColor: Colors.pink,
       ),
     );
   }
